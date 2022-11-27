e0d8c71e6a67090c89af94733c0f16f954c842bf
Krumpezz <lwfe.3241@gmail.com>
2022-11-27 17:47:39 +0530

    Initial commit

---
+++ src/rip/aspect/login/Login.java
@@ -0,0 +1,14 @@
+package rip.aspect.login;
+
+/*
+    Code by Aspect @ Uprising Studios
+    Project: login
+    Date: 11/27/2022 @ 5:28 PM
+*/
+
+public class Login {
+
+    public static void main(String[] args) { //Basic program startup logic
+
+    }
+}
